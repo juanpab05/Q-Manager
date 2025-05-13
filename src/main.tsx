f4f322141a7f70ab8c8f5a5dcccb4bcbb021cd68
Cobolios <ogcobolios@gmail.com>
2025-05-13 15:36:17 -0500

    Add @vercel/speed-insights dependency and integrate SpeedInsights component in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './styles/index.css'
 import App from './App'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <>
+    <SpeedInsights></SpeedInsights>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </>,
 )
